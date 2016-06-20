8c7de7a4a02a0655a754d752853668c0c2628575
S <s>
2016-06-20 15:38:14 -0700

    {"types":"C","runs":74}

--- Breakout.java
+++ Breakout.java
@@ -78,7 +78,7 @@ public class Breakout extends GraphicsProgram {
 		for(int i =1; i<=NBRICK_ROWS; i++){
 			
 			for(int j=0;j<NBRICKS_PER_ROW; j++){
-				GRect rect = new GRect(0+(j*(BRICK_WIDTH + BRICK_SEP)), bHeight , BRICK_WIDTH, BRICK_HEIGHT);
+				rect = new GRect(0+(j*(BRICK_WIDTH + BRICK_SEP)), bHeight , BRICK_WIDTH, BRICK_HEIGHT);
 				rect.setFilled(true);
 				if(i <= NBRICK_ROWS/5){ rect.setFillColor(Color.RED);}
 				else if(i > NBRICK_ROWS/5 && i <= 2*(NBRICK_ROWS/5)){rect.setFillColor(Color.ORANGE);}
@@ -116,7 +116,8 @@ public class Breakout extends GraphicsProgram {
 		while(true){	
 		moveBall();
 		pause(50);
-		addMouseListeners();}
+		addMouseListeners();
+		checkForCollison();}
 	}
 	
 	private void createBall(){
@@ -136,7 +137,34 @@ public class Breakout extends GraphicsProgram {
 		ball.move(vx, vy);
 	}
 	
+	public void checkForCollison(){
+		GObject collider = getCollidingObject();
+		if (collider == paddle){
+			vy = -vy;
+		}
+		if(collider == rect){
+			vy = -vy;
+		}
+	}
+	
+	public GObject getCollidingObject(){
+		GObject obj1;
+		while(true){
+		obj1 = getElementAt(ball.getX(), ball.getY());
+		if(obj1 != null){  break;}
+		obj1 = getElementAt (ball.getX(), ball.getY()+ 2*BALL_RADIUS);
+		if(obj1 != null){break;}
+		obj1 = getElementAt (ball.getX()+ 2*BALL_RADIUS, ball.getY());
+		if(obj1 != null){break;}
+		obj1 = getElementAt (ball.getX()+ 2*BALL_RADIUS, ball.getY()+ 2*BALL_RADIUS);
+		if(obj1 != null){break;}
+		obj1 = null;
+		break;
+		}
+		return obj1;
+	}
     //Instance Variables
+	public GRect rect;
 	public GRect paddle;
 	public double vx, vy;
 	public GOval ball;

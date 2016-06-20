e777464e2d6ab5483725b980b033d39bc3207a33
S <s>
2016-06-20 14:13:01 -0700

    {"types":"C","runs":69}

--- Breakout.java
+++ Breakout.java
@@ -128,7 +128,7 @@ public class Breakout extends GraphicsProgram {
 	}
 	
 	private void moveBall(){
-		vx += vx ; vy += 3;
+		vx += 2 ; vy += 3;
 		System.out.println("vx" +vx + "vy" + vy);
 		ball.move(vx, vy);
 		if(vy >getHeight() - BALL_RADIUS){ vy = -vy; System.out.println("vx if exceeds" + vy);}

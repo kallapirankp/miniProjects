4a9e4401b89e887dbdcca641a7b5cf5dbc520637
S <s>
2016-06-20 17:28:25 -0700

    {"types":"C","runs":128}

--- Breakout.java
+++ Breakout.java
@@ -118,9 +118,9 @@ public class Breakout extends GraphicsProgram {
 		vy = 3;
 		while(true){	
 		moveBall();
-		if(m<30)pause(5);
-		if(m>20 && m<40) pause(5);
-		if(m>40) pause(5);
+		if(m<30)pause(7);
+		if(m>20 && m<40) pause(7);
+		if(m>40) pause(7);
 		addMouseListeners();
 		checkForCollison();}
 	}
